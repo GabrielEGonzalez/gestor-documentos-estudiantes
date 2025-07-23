88f68e43cce7e80cf048eb88742dd3dd5a332bf6
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-07-23 14:45:42 -0500

    base de datos'

---
+++ bd.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE gestion;
+
+use gesion;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) UNIQUE NOT NULL,
+    pass VARCHAR(255) NOT NULL,
+    rol ENUM('estudiante', 'admin') DEFAULT 'estudiante'
+);
+
+CREATE TABLE archivos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_original VARCHAR(255) NOT NULL,
+    nombre_guardado VARCHAR(255) NOT NULL,
+    fecha_subida DATETIME DEFAULT CURRENT_TIMESTAMP,
+    usuario_id INT NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
