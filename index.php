7691af5826eb04a04d8b839be797600232939f53
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-07-23 14:46:56 -0500

    archivo index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>principal</title>
+</head>
+<body>
+    <div class="contenedor">
+        <div><a href="login.view.php">LoGin</a></div>
+        <div><a href="register.view.php">ReGisTer</a></div>
+    </div>
+</body>
+</html>
