245633a9f7603a57da156ab771a9241758b141ad
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-07-23 21:52:11 -0500

    modificacion de campo en la tabla archivos

--- bd.sql
+++ bd.sql
@@ -13,7 +13,7 @@ CREATE TABLE usuarios (
 CREATE TABLE archivos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre_original VARCHAR(255) NOT NULL,
-    nombre_guardado VARCHAR(255) NOT NULL,
+    guardado VARCHAR(255) NOT NULL,
     fecha_subida DATETIME DEFAULT CURRENT_TIMESTAMP,
     usuario_id INT NOT NULL,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
