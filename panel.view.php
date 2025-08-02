baa6297db626521cafbf9cb26123fcca136e0645
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-08-01 23:10:31 -0500

    modificacion de tabla de informacion

--- panel.view.php
+++ panel.view.php
@@ -1,11 +1,20 @@
 <?php
-    session_start();
+session_start();
 
-    if(!isset($_SESSION['usuario_id'])){
-        header('location: register.view.php');
-    }
+require_once './config/bd.php';
+if (!isset($_SESSION['usuario_id'])) {
+    header('location: register.view.php');
+}
+
+$mensaje = isset($_SESSION['mensaje'])? $_SESSION['mensaje']:'';
+
+$stmt = $conn->prepare('SELECT * FROM archivos WHERE usuario_id = ?');
+$stmt->bind_param('i', $_SESSION['usuario_id']);
+$stmt->execute();
+$resul = $stmt->get_result();
+$stmt->close();
+$archivos = $resul->fetch_all(MYSQLI_ASSOC);
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -13,16 +22,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel</title>
+
+    <style>
+        table {
+            border-collapse: collapse;
+            border: 2px solid #000;
+        }
+
+        td , th {
+            border: 1px solid black;
+            padding:10px;
+            text-align:center;
+        }
+    </style>
 </head>
 
 <body>
     
-    <form action="" method="post" enctype="multipart/form-data">
+    <?php echo $mensaje ;?>
+    <form action="uploads.php" method="post" enctype="multipart/form-data">
         <label for="envio">envio de documento:</label>
         <input type="file" name="envio" id="envio"><br>
         <h6>documentos con extencion .pdf .doc .docx</h6>
         <input type="submit" value="Enviar" onclick="startProgress()">
     </form>
+
+    <main class="con">
+        <h2>Lista de documentos</h2>
+        <table style="border:1px solid black;">
+            <thead>
+                <tr>
+                    <th style="border:1px solid black;">nombre archivo</th>
+                    <th style="border:1px solid black;">fecha archivo</th>
+                    <th style="border:1px solid black;">Descargar archivo</th>
+                    <th style="border:1px solid black;">eliminar archivos</th>
+                </tr>
+
+
+            </thead>
+            <tbody>
+                <?php foreach ($archivos as $ar):
+                    ; ?>
+                    <tr>
+                        <td> <?php echo $ar['nombre_original']; ?></td>
+                        <td> <?php echo $ar['fecha_subida']; ?></td>
+                        <td> <a href="<?php echo $ar['guardado']; ?>">Descargar</a></td>
+                        <td> <a href="<?php echo './delete.php?id=' . $ar['id']; ?>">Eliminar</a></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+
+        </table>
+    </main>
 
     <script>
         let poss = 0;
