cb6b653091e0732d81a1ca3d4f2ed55022fa733e
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-08-04 19:49:11 -0500

    mensaje de confirmacion por session

--- uploads.php
+++ uploads.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
     } else {
         if (move_uploaded_file($_FILES['envio']['tmp_name'], $filename)) {
-            $mensaje = '<div style="background-color: rgba(1, 194, 33, 0.5);color: #fff;width: 100%;height: 50px;"><h2>archivo enviando exitosamente.</h2></div>';
+            $mensaje = '<h2>archivo enviando exitosamente.</h2>';
             $stmt = $conn->prepare('INSERT INTO archivos(nombre_original,guardado,usuario_id) values(?,?,?);');
             $stmt->bind_param('sss',$_FILES['envio']['name'],$filename,$_SESSION['usuario_id']);
             $stmt->execute();
