d47a6fb1c79e39d71e5b036b2f499b0cd77fbe2d
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-08-06 21:43:09 -0500

    base  de datos nueva

--- config/bd.php
+++ config/bd.php
@@ -3,7 +3,7 @@
     $localhost ="127.0.0.1:3307";
     $username = "root";
     $password = "";
-    $bd_name="gestor_de_documentos";
+    $bd_name="gestordocumentos";
 
     $conn = new mysqli($localhost, $username, $password, $bd_name);
 
