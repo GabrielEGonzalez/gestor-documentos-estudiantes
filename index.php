82500bc5b501040742d0b2bb7ed729fd9e7a3811
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-08-07 21:31:58 -0500

    estilos y parrafos

--- index.php
+++ index.php
@@ -1,14 +1,38 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>principal</title>
+    <link rel="stylesheet" href="./css/index.css">
 </head>
+
 <body>
-    <div class="contenedor">
-        <div><a href="login.view.php">LoGin</a></div>
-        <div><a href="register.view.php">ReGisTer</a></div>
+    <header class="encabezado">
+        <h1>Gestor de Documentos Universitarios</h1>
+    </header>
+    <div class="cont1">
+        <div class="content">
+            <div class="imagen-fondo">
+                <img src="./img/freepik__the-style-is-candid-image-photography-with-natural__33405.png">
+            </div>
+            <div class="parrafos">
+                <p>Bienvenido a la plataforma donde los estudiantes pueden subir y gestionar sus documentos de forma
+                    segura. Los profesores tienen acceso a todos los archivos para su revisión.</p>
+            </div>
+        </div>
+
+        <div class="contenedor">
+            <div><a href="login.view.php">LoGin</a></div>
+            <div><a href="register.view.php">ReGisTer</a></div>
+        </div>
     </div>
+
+
+    <footer>
+        <p>Creado por Gabriel Gonzalez – 2025</p>
+    </footer>
 </body>
+
 </html>
